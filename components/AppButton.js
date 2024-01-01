d433319792380348066665d1854fd162ca5321e6
arisafari.ridwan <arisafari.ridwan@gmail.com>
2024-01-01 07:11:17 +0700

    retype lesson 6 - Component props

--- components/AppButton.js
+++ components/AppButton.js
@@ -2,14 +2,13 @@ export default {
   template: `
     <button
     :class= "{
-      'relative p-2 rounded-md disabled:cursor-not-allowed': true,
+      'relative p-2 rounded-md disabled:cursor-not-allowed':true,
       'bg-blue-400 hover:bg-blue-500': typebtn === 'primary',
       'bg-purple-400 hover:bg-purple-500': typebtn === 'secondary',
       'bg-slate-400 hover:bg-slate-500': typebtn === 'muted',
       'is-loading': processing
     }"
     :disabled= "processing"
-    @click="check"
     >
     <slot />
     </button>
@@ -24,12 +23,6 @@ export default {
     processing: {
       type: Boolean,
       default: false,
-    },
-  },
-
-  methods: {
-    check() {
-      this.props.processing['default'] = true;
     },
   },
 };
